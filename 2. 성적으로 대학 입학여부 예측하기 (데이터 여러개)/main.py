ca3ef5691b1ff2f7098a7899833214e6922551c5
시혁 박 <exon.indiv@gmail.com>
2022-07-19 21:49:45 +0900

    2

---
+++ 2. 성적으로 대학 입학여부 예측하기 (데이터 여러개)/main.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('gpascore.csv')
+data = data.dropna()
+
+yData = data['admit'].values
+xData = []
+
+for i, rows in data.iterrows():
+    xData.append([ rows['gre'], rows['gpa'], rows['rank'] ])
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(128, activation='sigmoid'),
+    tf.keras.layers.Dense(256, activation='sigmoid'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(np.array(xData), np.array(yData), epochs=1000)
+
+result = model.predict([[750, 3.70, 3], [400, 2.2, 1]])
+print(result)
