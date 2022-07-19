b939b2b72dd932df9b2dbec744284bee58eb2f95
시혁 박 <exon.indiv@gmail.com>
2022-07-19 18:24:42 +0900

    1

--- 1. 키로 몸무게 예측하기 (데이터 1개)/main.py
+++ 1. 키로 몸무게 예측하기 (데이터 1개)/main.py
@@ -8,7 +8,7 @@ a = tf.Variable(0.1)
 b = tf.Variable(0.2)
 
 def loss():
-    return tf.square(54 - (height * a + b))
+    return tf.square(weight - (height * a + b))
 
 opt = tf.keras.optimizers.Adam(learning_rate=0.1)
 
@@ -16,4 +16,5 @@ for i in range(300):
     opt.minimize(loss, var_list=[a, b])
     print(a.numpy(), b.numpy())
 
-print(178 * a.numpy() + b.numpy())
+n = int(input('자신의 키를 입력하세요: '))
+print(n * a.numpy() + b.numpy())
