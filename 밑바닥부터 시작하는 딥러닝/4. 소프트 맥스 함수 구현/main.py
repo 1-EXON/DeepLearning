04fb6129fe4a66a0730f9269170a72a4f40a9af5
1-EXON <exon.indiv@gmail.com>
2022-08-31 13:01:06 +0900

    4

--- 밑바닥부터 시작하는 딥러닝/4. 소프트 맥스 함수 구현/main.py
+++ 밑바닥부터 시작하는 딥러닝/4. 소프트 맥스 함수 구현/main.py
@@ -6,4 +6,13 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y
 
-print(softmax(np.array([32, 12, 20])))
+print(softmax(np.array([1010, 1000, 990]))) # 오버플로우 문제
+
+def newSoftmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+print(newSoftmax(np.array([1010, 1000, 990]))) # 오버플로우 문제 해결
